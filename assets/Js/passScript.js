6896e4c9456c78fe69c7f74f72cc055d258f9e06
Richard Germaine <rlgerma92@outlook.com>
2019-09-29 12:05:42 -0600

    another one

--- assets/Js/passScript.js
+++ assets/Js/passScript.js
@@ -9,16 +9,25 @@ var newPass = "";
 var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var length = 0-32;
 
-var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes followed by an 'enter' key to get started");
+// var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes followed by an 'enter' key to get started");
 
-var charSize = prompt("How many characters long will your password be?")
+// var charSize = prompt("How many characters long will your password be?")
+
+// var specChar = prompt("Would you like special characters in your password?");
+
+// var numChar = prompt("How about adding some numbers too?");
+
+// var capChar = prompt("Should it be case sensitive?");
+
+var input = document.querySelector('#textarea')
 
 next.addEventListener("keydown", function (e){
     var key = e.keyCode
 if (e.key === 13){
-    return input;
+    console.log(input)
 }
 });
+
 
 function passwordGen() {
     
