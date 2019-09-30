98ab42746de57ad404ce79ffcd9093adda0afbf0
Richard Germaine <rlgerma92@outlook.com>
2019-09-29 20:28:57 -0600

    update

--- assets/Js/passScript.js
+++ assets/Js/passScript.js
@@ -8,7 +8,10 @@ var num2 = 32;
 
 var yes = true;
 
+var textAreaEl = document.getElementById("form[3]");
+
 function theNewPass(){
+    
     console.log(size)
     console.log(symbolAsk)
 }
@@ -19,55 +22,68 @@ function passwordGen() {
     length = size;
     newPass = "";
     
+    
     for (var i = 0, n = charSet.length; i < length; ++i) {
         newPass += charSet.charAt(Math.floor(Math.random() * n));
         console.log(i);    
     }
-    return newPass;
+    
+    return (newPass);
 };
-function passwordGenSymbols() {
+
+function passwordGenSymbol() {
     
     symbols = "!@#{[]\:;?$%^&*()_+~`|}><,./-=';",
     charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     length = size;
     newPass = "";
+    newPassSymbol = "";
     
-    for (var i = 0, n = charSet.length + symbols.length; i < length; ++i) {
-        newPass += symbols.charAt(Math.floor(Math.random() * n));
+    for (var i = 0, n = charSet.length; i < length; ++i) {
+        newPassSymbol += charSet.charAt(Math.floor(Math.random() * n));
         console.log(i);    
     }
-    return newPass;
+    for (var i = 0, s = symbols.length; i < length; ++i) {
+        newPassSymbol += symbols.charAt(Math.floor(Math.random() * s));
+        console.log(i);    
+    }
+    return (newPassSymbol);
 };
 
-var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes either 'yes' or 'no'");
+
+
+var welcome = confirm("Welcome to Password Generator! Please enter all information in the boxes either 'yes' or 'no'");
 if (welcome == true){    
     size = prompt("How many characters long will your password be? 5-32 characters accepted");
-    if (size >= 5 && size <= 32){
-        passwordGen();
+    if (size >= 5 && size <= 32){        
         theNewPass();
+          
+    }
+        
+    else {
+        alert("needs to be 5-32 numbers");
+        
+        
+    } 
+    
+    var symbolAsk = prompt("Want special characters?");
+    if (symbolAsk == "yes"){
+        passwordGenSymbol();
+        theNewPass();        
+        Element.setAttribute(textAreaEl, newPassSymbol);
+        
         
         
     }
-        
-}
-
-var symbolAsk = prompt("Want special characters?");
-if (symbolAsk === "yes"){
-    passwordGenSymbols();
-    theNewPass();
-    alert(newPass)
+    else if (symbolAsk == "no"){
+        passwordGen();
+        alert(newPass)
+    }
     
+    else if (welcome || size || symbolAsk !== true) {
+        alert("Maybe another time!")
     
-}
-
-else if (length !== true){
-    alert("needs to be 5-32 numbers");
-    
-    
-} 
-else if (welcome || length !== true) {
-    alert("Maybe another time!")
-    
+    }
 }
 // passwordGen();
 // alert(newPass)    
