2bafe0e3ffa99026307b7eb36cdd439e7b75f048
Richard Germaine <rlgerma92@outlook.com>
2019-09-29 18:05:47 -0600

    updates

--- assets/Js/passScript.js
+++ assets/Js/passScript.js
@@ -1,40 +1,43 @@
-// Create array for allowed letters, numbers, and symbols in password
-// Create objects with functions for each button
-// Create go to bed........
 
 // var textAreaEl = document.getElementById("#password").autofocus;
 
-var symbols = "!@#{[]\:;?$%^&*()_+~`|}><,./-=';";
-var newPass = "";
-var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var length = 0-32;
+var welcome;
 
-// var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes followed by an 'enter' key to get started");
+var num1 = 5;
+var num2 = 32;
 
-// var charSize = prompt("How many characters long will your password be?")
-
-// var specChar = prompt("Would you like special characters in your password?");
-
-// var numChar = prompt("How about adding some numbers too?");
-
-// var capChar = prompt("Should it be case sensitive?");
-
-var input = document.querySelector('#textarea')
-
-next.addEventListener("keydown", function (e){
-    var key = e.keyCode
-if (e.key === 13){
-    console.log(input)
+function theNewPass(){
+    console.log(newPass)
 }
-});
-
-
 function passwordGen() {
     
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        newPass += charset.charAt(Math.floor(Math.random() * n));
+    symbols = "!@#{[]\:;?$%^&*()_+~`|}><,./-=';",
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    length = size;
+    newPass = "";
+    
+    for (var i = 0, n = charSet.length; i < length; ++i) {
+        newPass += charSet.charAt(Math.floor(Math.random() * n));
+        console.log(i);    
     }
-    return retVal;
+    return newPass;
+};
+
+var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes either 'yes' or 'no'");
+if (welcome == true){    
+    size = prompt("How many characters long will your password be? 5-32 characters accepted");
+    if (size >= 5 && size <= 32){
+        passwordGen();
+        alert(newPass)    
+    }
+    else if (length !== true){
+        alert("needs to be 5-32 numbers");
+        
+    } 
+        
+        
 }
-
-
+else if (welcome || length !== true) {
+    alert("Maybe another time!")
+    
+}
