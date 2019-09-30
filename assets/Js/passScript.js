0c722c817be843e74758711983dc1cbd90e83ab0
Richard Germaine <rlgerma92@outlook.com>
2019-09-29 18:44:21 -0600

    update

--- assets/Js/passScript.js
+++ assets/Js/passScript.js
@@ -6,8 +6,11 @@ var welcome;
 var num1 = 5;
 var num2 = 32;
 
+var yes = true;
+
 function theNewPass(){
-    console.log(newPass)
+    console.log(size)
+    console.log(symbolAsk)
 }
 function passwordGen() {
     
@@ -22,22 +25,49 @@ function passwordGen() {
     }
     return newPass;
 };
+function passwordGenSymbols() {
+    
+    symbols = "!@#{[]\:;?$%^&*()_+~`|}><,./-=';",
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    length = size;
+    newPass = "";
+    
+    for (var i = 0, n = charSet.length + symbols.length; i < length; ++i) {
+        newPass += symbols.charAt(Math.floor(Math.random() * n));
+        console.log(i);    
+    }
+    return newPass;
+};
 
 var welcome = confirm("Welcome to Password Generator", "Please enter all information in the boxes either 'yes' or 'no'");
 if (welcome == true){    
     size = prompt("How many characters long will your password be? 5-32 characters accepted");
     if (size >= 5 && size <= 32){
         passwordGen();
-        alert(newPass)    
+        theNewPass();
+        
+        
     }
-    else if (length !== true){
-        alert("needs to be 5-32 numbers");
-        
-    } 
-        
         
 }
+
+var symbolAsk = prompt("Want special characters?");
+if (symbolAsk === "yes"){
+    passwordGenSymbols();
+    theNewPass();
+    alert(newPass)
+    
+    
+}
+
+else if (length !== true){
+    alert("needs to be 5-32 numbers");
+    
+    
+} 
 else if (welcome || length !== true) {
     alert("Maybe another time!")
     
 }
+// passwordGen();
+// alert(newPass)    
