1536acc5f8f4e1cf0ef1b3790d2e83ef791fa381
Richard Germaine <rlgerma92@outlook.com>
2019-12-05 15:35:08 -0700

    final update. ver 1.0 ready

--- assets/Js/passScript.js
+++ assets/Js/passScript.js
@@ -1,16 +1,13 @@
-var welcome;
-
 var num1 = 8;
 var num2 = 128;
 
 var yes = true;
 
-function theNewPass(){
-    
+function theNewPass(){    
     console.log(size)
+    console.log(capsAsk)
     console.log(symbolAsk)
 }
-
 function passwordGen() {
     
     charSet = "abcdefghijklmnopqrstuvwxyz";
@@ -24,7 +21,6 @@ function passwordGen() {
     
     return (newPass);
 };
-
 function passwordGenCaps() {
     
     charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -39,7 +35,6 @@ function passwordGenCaps() {
     
     return (newPassCaps);
 };
-
 function passwordGenSymbol() {
     
     charSet = "abcdefghijklmnopqrstuvwxyz!@#{[]\:;?$%^&*()_+~`|}><,./-=';";
@@ -51,24 +46,21 @@ function passwordGenSymbol() {
         newPassSymbol += charSet.charAt(Math.floor(Math.random() * n));
         console.log(i);    
     }
-
     return (newPassSymbol);
 };
-
 function passwordGenSymbolCaps() {
     charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#{[]\:;?$%^&*()_+~`|}><,./-=';";
     length = size;
     newPass = "";
-    newPassgernSymbolCaps = "";
+    newPassSymbolCaps = "";
         
     for (var i = 0, n = charSet.length; i < length; ++i) {
-        newPass += charSet.charAt(Math.floor(Math.random() * n));
+        newPassSymbolCaps += charSet.charAt(Math.floor(Math.random() * n));
         console.log(i);    
     }
     
     return (newPassSymbolCaps);
 };
-
 function copyTextEl() {
     var copyText = document.getElementById("password");
     copyText.select();
@@ -76,7 +68,6 @@ function copyTextEl() {
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
 };
-
 var welcome = confirm("Welcome to Password Generator! Please enter all information in the boxes either 'yes' or 'no'");
 if (welcome == true){    
     size = prompt("How many characters long will your password be? 8-128 characters accepted");
@@ -88,6 +79,7 @@ if (welcome == true){
         .clearfix();
     }
     var capsAsk = prompt("Would You like Capital letters included?(default is no caps)");
+
     if(capsAsk == "yes" && size >= 8 && size <= 128){
         passwordGenCaps();
         theNewPass();
@@ -97,24 +89,25 @@ if (welcome == true){
     else alert("no caps, no problems!")
 
     var symbolAsk = prompt("Want special characters?");
+
     if (symbolAsk && capsAsk == "yes" && size >= 8 && size <= 128){
-        passwordGenSymbol();
+        passwordGenSymbolCaps();
         theNewPass();        
-        document.getElementById("password").innerHTML = newPassSymbol;
+        document.getElementById("password").innerHTML = newPassSymbolCaps;
     }
     else if (size < 8 && size > 128 == true){
         alert("nice try!");
     }
      if (symbolAsk == "no" && capsAsk == "yes" && size >= 8 && size <= 128){
-        passwordGenCaps();        
-        document.getElementById("password").innerHTML = newPass;
+        passwordGenCaps(); 
+        theNewPass();       
+        document.getElementById("password").innerHTML = newPassCaps;
        }
     else if (symbolAsk == "yes" && capsAsk == "no" && size >= 8 && size <= 128){
-        passwordGenSymbol();        
-        document.getElementById("password").innerHTML = newPass;
+        passwordGenSymbol();
+        theNewPass();        
+        document.getElementById("password").innerHTML = newPassSymbol;
        }
-    else if (welcome || size < 8 && size > 128 == true) {
-        alert("Enjoy!");
-    }
+    
+    alert("Enjoy!");
 };
-   
---
+++ readme.md
@@ -0,0 +1,11 @@
+**Password Generator**
+
+This application is a resource for generating a secure password.
+
+The password generator walks a user through a series of prompts asking for character length (8-128), caps or no caps, and if they want to use special charecters.
+
+Basic html, js and css was used for this.
+
+inquiries about this app can be made via email to rlgerma92@outlook.com
+
+Enjoy!
